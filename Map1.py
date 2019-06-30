949a3910af32d8baf9236f6250c96062367f049c
jesseking96 <jesseking96@yahoo.com>
2019-06-30 10:24:57 -0500

    converted coordinates and elevation of volcanoes into python lists, added them to the map

--- Map1.py
+++ Map1.py
@@ -11,12 +11,16 @@ import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
 
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
 map = folium.Map(location = [38.58, -99.09], zoom_start = 6)
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
