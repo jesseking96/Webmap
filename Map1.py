cb67b5b56722a7ff1ab5db4771583886fa133e78
jesseking96 <jesseking96@yahoo.com>
2019-07-01 06:51:55 -0500

    Added location markers to the map for the volcanoes, and assigned colors to the markers based on elevation.

--- Map1.py
+++ Map1.py
@@ -15,13 +15,22 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elev):
+    if elev < 1000:
+        return 'green'
+    elif 1000 <= elev < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location = [38.58, -99.09], zoom_start = 6)
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 6, popup=str(el) + " m", fill_color=color_producer(el), color = "grey", fill_opacity = 0.7))
 
 map.add_child(fg)
 
 map.save("Map.html")
+
